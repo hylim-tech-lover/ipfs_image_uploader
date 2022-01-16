4cce0066fbaa6dc878c3a799d7e1ee578f9c26f6
HY Lim <hylim@HYs-MacBook-Air.local>
2022-01-16 18:38:10 +0800

    Add changes to finalize on IPFS based on guideline

--- client/src/App.js
+++ client/src/App.js
@@ -6,42 +6,46 @@ import ipfs from './ipfs';
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null, ipfsHash: '' };
+  state = { web3: null, accounts: null, contract: null, ipfsHash: '' };
 
   captureFile = this.captureFile.bind(this);
   onSubmit = this.onSubmit.bind(this);
 
   componentDidMount = async () => {
-    // try {
-    //   // Get network provider and web3 instance.
-    //   const web3 = await getWeb3();
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
 
-    //   // Use web3 to get the user's accounts.
-    //   const accounts = await web3.eth.getAccounts();
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
 
-    //   // Get the contract instance.
-    //   const networkId = await web3.eth.net.getId();
-    //   const deployedNetwork = SimpleStorageContract.networks[networkId];
-    //   const instance = new web3.eth.Contract(
-    //     SimpleStorageContract.abi,
-    //     deployedNetwork && deployedNetwork.address,
-    //   );
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
 
-    //   // Set web3, accounts, and contract to the state, and then proceed with an
-    //   // example of interacting with the contract's methods.
-    //   this.setState({ web3, accounts, contract: instance }, this.runExample);
-    // } catch (error) {
-    //   // Catch any errors for any of the above operations.
-    //   alert(
-    //     `Failed to load web3, accounts, or contract. Check console for details.`,
-    //   );
-    //   console.error(error);
-    // }
+      // Use the smart contract instance get function that get from account 0
+      const response = await instance.methods.get().call({ from: accounts[0] });
+      //console.log('ipfsHash:', response)
+
+      // Set web3, accounts, and contract, ipfsHash to the state.
+      this.setState({ web3, accounts, contract: instance, ipfsHash: response });
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
   };
 
   captureFile(event) {
+    // Prevent event default behavior
     event.preventDefault()
-    // Get loaded file and stored in file
+    // Get loaded file and stored in file variable
     const file = event.target.files[0]
     // Node API to read file
     const reader = new window.FileReader()
@@ -50,7 +54,7 @@ class App extends Component {
     reader.onloadend = () => {
       // Store buffer to state in React
       this.setState({ buffer: Buffer(reader.result) })
-      console.log('result of raw buffer:', this.state.buffer)
+      //console.log('result of raw buffer:', this.state.buffer)
     }
   }
 
@@ -63,27 +67,16 @@ class App extends Component {
         return
       }
       this.setState({ ipfsHash: result[0].hash })
-      console.log('ipfs hash:',this.state.ipfsHash)
+      console.log('ipfs hash:', this.state.ipfsHash);
+      this.state.contract.methods.set(this.state.ipfsHash).send({ from: this.state.accounts[0] });
+      this.state.contract.methods.get().call(this.state.accounts[0]);
     })
   }
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
   render() {
-    // if (!this.state.web3) {
-    //   return <div>Loading Web3, accounts, and contract...</div>;
-    // }
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
     // img src with string manipulation with {`{react_state}`}
     return (
       <div className="App">
@@ -91,18 +84,10 @@ class App extends Component {
         <p>This image is stored on IPFS and the Ethereum Blockchain.</p>
         <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt="picture" />
         <h2>Upload Image</h2>
-        <form onSubmit={this.onSubmit} >
+        <form onSubmit={this.onSubmit} margin="5" >
           <input type='file' onChange={this.captureFile} />
           <input type='submit' />
         </form>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
       </div>
     );
   }
